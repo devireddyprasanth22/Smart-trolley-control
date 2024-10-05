8d499f816a945ff3cbd2bedd6c4e7d621ed422f7
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-10-05 11:57:01 +1000

    new code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import "./style.css";
+
+const forwardButton = document.querySelector(".control_straight") as HTMLImageElement;
+const leftButton = document.querySelector(".control_left") as HTMLImageElement;
+const rightButton = document.querySelector(".control_right") as HTMLImageElement;
+const backButton = document.querySelector(".control_back") as HTMLImageElement;
+
+forwardButton.addEventListener("click", () => {
+    console.log("forward");
+});
+
+leftButton.addEventListener("click", () => {
+    console.log("left");
+});
+
+rightButton.addEventListener("click", () => {
+    console.log("right");
+});
+
+backButton.addEventListener("click", () => {
+    console.log("back");
+});
